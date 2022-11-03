cc964f59f7bfd24584e1e894bcd04933a07b4a84
Ashley Cave <45659989+AHollowedHunter@users.noreply.github.com>
2022-11-03 17:30:05 +0000

    [wip] map and location working
    Sort out state etc. and updates

--- discover-app/components/Map.tsx
+++ discover-app/components/Map.tsx
@@ -1,42 +1,65 @@
 import React from "react";
 import MapView, { Marker } from "react-native-maps";
 
-import * as Location from "expo-location";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObjectCoords,
+} from "expo-location";
 
 export interface MapProps {
   children: React.ReactNode[];
 }
 
 export interface MapState {
-  location: Location.LocationObject;
+  location: LocationObjectCoords;
 }
 
-export default class Map extends React.Component<MapProps, {}> {
+export default class Map extends React.Component<MapProps, MapState> {
   public static defaultProps = {
     children: [],
   };
 
-  // constructor(props: MapProps) {
-  //   super(props);
+  constructor(props: MapProps) {
+    super(props);
 
-  //   this.state = {
-  //     location: new ()
-  //   };
-  // }
+    this.state = {
+      location: {
+        longitude: 50.6884,
+        latitude: -1.95622,
+      } as LocationObjectCoords,
+    };
+  }
+
+  async componentDidMount(): Promise<void> {
+    let { status } = await requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      // setErrorMsg("Permission to access location was denied");
+      return;
+    }
+
+    let position = await getCurrentPositionAsync();
+    this.state = {
+      location: position.coords,
+    };
+  }
 
   public render(): React.ReactNode {
     return (
       <MapView
         style={{ flex: 1 }}
         initialRegion={{
-          latitude: 50.813676,
-          longitude: -2.4747,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
+          latitude: this.state.location.latitude,
+          longitude: this.state.location.longitude,
+
+          // latitude: 50.6884,
+          // longitude: -1.95622,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
         }}
         showsUserLocation={true}
-        followsUserLocation={true}
-        // mapType="hybrid"
+        // followsUserLocation={true}
+        mapType="standard"
       >
         {this.props.children}
       </MapView>
